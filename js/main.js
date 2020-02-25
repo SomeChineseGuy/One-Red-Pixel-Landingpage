fbe2c56b2a7b7ad54a0c1502d6f93a85c84ba542
Alvin Ng <alvin.cl.ng@gmail.com>
2020-02-24 16:04:00 -0800

    removed unused files, added venmo video and instagram

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 $(document).ready(function() {
-    
-
     // All init files
-     new WOW().init();
-     var rellax = new Rellax('.rellax', {
+    //  new WOW().init();
+    //  var rellax = new Rellax('.rellax', {
+    // });
 
-    });
+    setTimeout(() => {
+        $("#video #eapps-instagram-feed-1 a[href$='https://elfsight.com/instagram-feed-instashow/?utm_source=websites&utm_medium=clients&utm_content=instagram-feed&utm_term=undefined&utm_campaign=free-widget']").hide();
+    }, 1000);
+    
 });
+
+
 
