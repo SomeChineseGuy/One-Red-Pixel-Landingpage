bf3cc54fc444e7b4f726a0c62efa7c1f7c71ad38
Alvin Ng <alvin.cl.ng@gmail.com>
2020-02-25 16:44:47 -0800

    change target name and rename section

--- js/main.js
+++ js/main.js
@@ -3,12 +3,8 @@ $(document).ready(function() {
     //  new WOW().init();
     //  var rellax = new Rellax('.rellax', {
     // });
-
     setTimeout(() => {
-        $("#video #eapps-instagram-feed-1 a[href$='https://elfsight.com/instagram-feed-instashow/?utm_source=websites&utm_medium=clients&utm_content=instagram-feed&utm_term=undefined&utm_campaign=free-widget']").hide();
+        var widget = $("#instagram #eapps-instagram-feed-1 > a[href$='https://elfsight.com/instagram-feed-instashow/?utm_source=websites&utm_medium=clients&utm_content=instagram-feed&utm_term=undefined&utm_campaign=free-widget']")
+        widget.hide()
     }, 1000);
-    
 });
-
-
-
