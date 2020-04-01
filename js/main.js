71009bd43bf23b3d99a5549e3e5293983c55a4a9
Alvin Ng <alvin.cl.ng@gmail.com>
2020-04-01 15:30:57 -0700

    removed accordion section

--- js/main.js
+++ js/main.js
@@ -8,13 +8,13 @@ $(document).ready(function() {
     setTimeout(() => {
         var widget = $("#instagram #eapps-instagram-feed-1 > a[href$='https://elfsight.com/instagram-feed-instashow/?utm_source=websites&utm_medium=clients&utm_content=instagram-feed&utm_term=undefined&utm_campaign=free-widget']")
         widget.hide()
-    }, 1000);
+    }, 3000);
 
     // Accordion
-    $('.accordion').click(function(){
-        $(this).toggleClass('open');
-        $(this).hasClass('open') ? $(".accordion-content", this).slideDown() : $(".accordion-content", this).slideUp();
-        $(this).hasClass('open') ? $(".btn", this).slideUp() : $(".btn", this).slideDown();
-    })
+    // $('.accordion').click(function(){
+    //     $(this).toggleClass('open');
+    //     $(this).hasClass('open') ? $(".accordion-content", this).slideDown() : $(".accordion-content", this).slideUp();
+    //     $(this).hasClass('open') ? $(".btn", this).slideUp() : $(".btn", this).slideDown();
+    // })
     
 });
